133c7374e196bb929d068c4f37a74a8b5c04489e
Taylor Georgia <taylor@taybird.com>
2014-07-27 15:47:18 +0000

    Removed fluff

--- __init__.py
+++ __init__.py
@@ -12,11 +12,6 @@ app.config['STORMPATH_APPLICATION'] = '***REMOVED***'
 app.config['STORMPATH_LOGIN_TEMPLATE'] = 'login.html'
 app.config['STORMPATH_ENABLE_REGISTRATION'] = False
 
-#app.config['SECRET_KEY'] = '***REMOVED***'
-#app.config['STORMPATH_API_KEY_ID'] = "***REMOVED***"
-#app.config['STORMPATH_API_KEY_SECRET'] = "***REMOVED***"
-#app.config['STORMPATH_APPLICATION'] = '***REMOVED***'
-
 user_manage = StormpathManager(app)
 
 import views
