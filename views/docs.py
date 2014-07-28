721320f4071665d87ad24ea3e5d578c10dcb0f3a
Taylor Georgia <taylor@taybird.com>
2014-07-28 16:11:14 -0600

    Added file upload and saving

--- views/docs.py
+++ views/docs.py
@@ -1,19 +1,37 @@
-""" Server the document manager. """
+""" Serve the document manager. """
+
+import os
 
 from flask import render_template, url_for, request, redirect
 from flask.ext.stormpath import login_required
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
 from interface import app, config
-app.config["SQLALCHEMY_DATABASE_URI"] = config["DOC_MANAGER"]["SQLALCHEMY_DATABASE_URI"]
-app.config["UPLOADS_FOLDER"] = config["DOC_MANAGER"]["UPLOADS_FOLDER"]
+
+config = config["DOC_MANAGER"]
+app.config["SQLALCHEMY_DATABASE_URI"] = config["SQLALCHEMY_DATABASE_URI"]
+app.config["UPLOADS_FOLDER"] = config["UPLOADS_FOLDER"]
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+    """ Check for a valid file type. """
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route("/docs", methods=['GET', 'POST'])
 @login_required
 def docs():
+    """ Serve the upload page and accept uploads. """
     if request.method == 'POST':
-        #save(request.files['upload'])
-        return redirect(url_for('docs'))
+        print request.form['name']
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOADS_FOLDER'], filename))
+            return redirect(url_for('docs'))
     return render_template('docs.html')
 
 @app.route("/upload")
