2596a7e1cbf5702ed4cd9b81a1845cc1cbc26d75
Taylor Georgia <taylor@taybird.com>
2014-07-28 12:58:07 -0600

    Email checking now accounts for an empty mail.json file.

--- views/dash.py
+++ views/dash.py
@@ -13,32 +13,36 @@ from interface import app, config
 @app.route("/dash")
 @login_required
 def dash():
-	# Get Apache uptime. Not currently used. Add to full stat view?
-	now = time()
-	boot = Process(getpid()).create_time()
-	apache_uptime = (now - boot) / 3600  # Convert to hours
-	
-	# Get server uptime.
-	with open('/proc/uptime', 'r') as f:
-		uptime_s = float(f.readline().split()[0])
-		uptime = uptime_s / 3600
-	
-	# Get disk usage.
-	usage = disk_usage('/').percent
-	
-	# Get open network connections.
-	ip = net_connections()
-	cons = []
-	for x in ip:
-		if x.status is not 'NONE' and x.status is not 'LISTEN':
-			con = x.raddr + (x.status,)
-			cons.append(con)
-	
-	# Get mail count in inbox.
-	f = open(config["EMAIL"]["MAIL_STORAGE"], "r")
-	mail = f.read()
-	f.close()
-	mail_count = len(json.loads(mail))
+    # Get Apache uptime. Not currently used. Add to full stat view?
+    now = time()
+    boot = Process(getpid()).create_time()
+    apache_uptime = (now - boot) / 3600  # Convert to hours
 
-	return render_template('dash.html', uptime=str(uptime)[:5], usage=usage,
-			       cons=cons, mail_count=mail_count)
+    # Get server uptime.
+    with open('/proc/uptime', 'r') as f:
+        uptime_s = float(f.readline().split()[0])
+        uptime = uptime_s / 3600
+
+    # Get disk usage.
+    usage = disk_usage('/').percent
+
+    # Get open network connections.
+    ip = net_connections()
+    cons = []
+    for x in ip:
+        if x.status is not 'NONE' and x.status is not 'LISTEN':
+            con = x.raddr + (x.status,)
+            cons.append(con)
+
+    # Get mail count in inbox.
+    f = open(config["EMAIL"]["MAIL_STORAGE"], "r")
+    mail = f.read()
+    f.close()
+
+    try:
+        mail_count = len(json.loads(mail))
+    except ValueError:
+        mail_count = 0
+
+    return render_template('dash.html', uptime=str(uptime)[:5], usage=usage,
+                   cons=cons, mail_count=mail_count)
--- views/email.py
+++ views/email.py
@@ -11,10 +11,14 @@ from interface import app, config
 
 @app.route("/email")
 @login_required
-def email():	
-	f = open(config["EMAIL"]["MAIL_STORAGE"],"r")
-	raw_mail = f.read()
-	f.close()
-	mail = json.loads(raw_mail)
-		
-	return render_template('email.html', mail=mail)
+def email():
+    f = open(config["EMAIL"]["MAIL_STORAGE"],"r")
+    raw_mail = f.read()
+    f.close()
+    
+    try:
+        mail = json.loads(raw_mail)
+    except:
+        mail = {}
+    
+    return render_template('email.html', mail=mail)
